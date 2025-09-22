1daff14337c705f8510548b9df954d67a4b63460
onuma <djp-780-gpv@e-net.nara.jp>
2025-09-22 06:20:27 +0000

    球を撃つ

---
+++ game/main.js
@@ -0,0 +1,76 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+
+const player = {
+    x: canvas.width / 2 - 15,
+    y: canvas.height - 60,
+    width: 30,
+    height: 30,
+    color: "yellow",
+    life: 3,
+};
+
+const bullets=[];
+const BULLET_SPEED=-10;
+
+function tryShoot(){
+    bullets.push({
+        x:player.x,
+        y:player.y,
+        width:5,
+        height:5,
+        vy:BULLET_SPEED,
+    })
+}
+        
+ 
+// fillRect(x座標(横), y座標(縦), 横幅, 縦幅)
+
+//自分のキャラクターに見立てた四角形をとりあえず作ってみてください。
+
+// width="480" height="640"
+
+window.addEventListener("keydown", (e) => {
+    if (e.key === "ArrowLeft") {
+     if( player.x -= 10;)
+        
+    } else if (e.key === "ArrowRight") {
+     player.x += 10;
+    } else if (e.code === "Space") {
+     tryShoot();
+    }
+});
+
+function update() {
+      for(let i=bullets.length-1; i>=0; i++){
+        const bullet=bullets[i];
+        bullet.y += bullet.vy;
+        if(bullet.y < 0){
+            bullets.splice(i,1);
+        }
+    }
+}
+
+function draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.fillStyle = player.color;
+    ctx.fillRect(player.x,player.y,player.width,player.height);
+    
+    ctx.fillStyle="white";
+    for(let i=bullets.length-1; i>=0; i--){
+        const bullet=bullets[i];
+        bullet.y += bullet.vy;
+        ctx.fillRect(bullet.x,bullet.y,bullet.width,bullet.height);
+        
+    }
+    
+    function gameLoop() {
+        update();
+        draw();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
